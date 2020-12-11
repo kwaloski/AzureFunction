023d3e8198ed37697f0bd6b52363d813d8601df1
kwaloski <40489017+kwaloski@users.noreply.github.com>
2020-12-11 12:54:22 +0530

    Update Function1.cs

--- DemoFunction/DemoFunction/Function1.cs
+++ DemoFunction/DemoFunction/Function1.cs
@@ -27,9 +27,8 @@ namespace DemoFunction
 
             //string responseMessage = string.IsNullOrEmpty(name)
             //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult("Thank you");
+            //    : $"Hello, {name}. This HTTP triggered function executed successfully."
+          return new OkObjectResult("Thank you");
         }
     }
 }
